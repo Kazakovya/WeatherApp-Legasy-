001a7f7eefc1cbd7933589b2bc7d4287ba88da69
Kazakov_Yury <kazakov.y@gmail.com>
2021-06-14 21:18:37 +0300

    lesson2

---
+++ app/src/main/java/ru/geekbrains/kazakovya/weatherapp/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.geekbrains.kazakovya.weatherapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.kazakovya.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
