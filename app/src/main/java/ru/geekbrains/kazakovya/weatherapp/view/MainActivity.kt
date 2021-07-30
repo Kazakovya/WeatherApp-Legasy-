f80342ddf06c19db0cbad3b4ecd830e5033c6381
Kazakov_Yury <kazakov.y@gmail.com>
2021-07-30 20:53:09 +0300

    5

--- app/src/main/java/ru/geekbrains/kazakovya/weatherapp/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/kazakovya/weatherapp/view/MainActivity.kt
@@ -3,13 +3,18 @@ package ru.geekbrains.kazakovya.weatherapp.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.geekbrains.kazakovya.weatherapp.R
+import ru.geekbrains.kazakovya.weatherapp.databinding.MainActivityBinding
 import ru.geekbrains.kazakovya.weatherapp.view.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.ok.setOnClickListener(clickListener)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
